1e913559ab848c87b540601db1bff4c9bb694aa4
Leonardo Cesar <coderleonardo@gmail.com>
2022-12-08 15:33:13 -0300

    naive probability

--- 01_statistical_inference/01_probability_theory/01_counting/naive_probability.R
+++ 01_statistical_inference/01_probability_theory/01_counting/naive_probability.R
@@ -1,3 +1,6 @@
+#######################################################################
+# COUNTING
+
 # define the vectors for size, topping and order
 size = c("S", "M", "L")
 topping = c("pepperoni", "sausage", "meatball", "extra cheese")
@@ -19,3 +22,31 @@ for(i in 1:length(size)){
 
 # print out the pizzas; should have 24
 pizzas
+
+# count total number of large sausages; should get 2
+#   we divide by 3 because rows are length 3, and we want
+#   to convert back to number of rows (i.e., number of pizzas)
+pizzas[pizzas[ ,1] == "L" & pizzas[ ,2] == "sausage"]
+length(pizzas[pizzas[ ,1] == "L" & pizzas[ ,2] == "sausage"]) / 3
+
+#######################################################################
+# FACTORIAL: counting the number of ways to order n elements without replacement
+
+# generate all of the possible permutations
+install.packages("combinat")
+perms = combinat::permn(c("A", "B", "C", "D", "E", "F", "G"))
+
+# look at the first few permutations
+head(perms)
+
+# should get factorial(7) = 5040
+length(perms)
+
+# example
+# generate the committees (people labeled 1 to 5)
+install.packages("gtools")
+committees = gtools::combinations(n = 5, r = 3)
+
+# should get choose(5, 3) = factorial(5)/(factorial(3)*factorial(2)) = 10 committees
+committees
+#######################################################################
